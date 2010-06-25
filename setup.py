47bc521cb0e02bf23e3fd51e561b0273b91191d6
ext <ext@sidvind.com>
2010-06-25 15:06:52 +0200

    bsd license and setuptools

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+setup(
+    name = "htmlcolor",
+    version = "0.1",
+    packages = find_packages(),
+    
+    author = "David Sveningsson",
+    author_email = "ext@sidvind.com",
+    description = "Parsing HTML and CSS color values",
+    license = "BSD",
+    keywords = "color html css",
+    url = "http://github.com/ext/pyhtmlcolor",
+)
