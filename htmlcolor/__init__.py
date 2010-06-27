211eb1a8ec61f892c5068175e2dd9db13082b92b
ext <ext@sidvind.com>
2010-06-27 18:49:43 +0200

    moving test code out of package

--- htmlcolor/__init__.py
+++ htmlcolor/__init__.py
@@ -219,25 +219,3 @@ class test(unittest.TestCase):
     
     def test_invalid_name(self):
         self.assertRaises(ValueError, self.p.parse, 'foobar')
-
-if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromNames(['htmlcolor.test', 'htmlcolor.factory_test', 'htmlcolor.components_test'])
-    unittest.TextTestRunner(verbosity=2).run(suite)
-    
-    print
-    print ' *** Sample usage ***'
-    
-    values = ['#ff7700', 'ff7700', '#f70', '#ff770077', 'hotpink']
-    parser = Parser()
-    
-    print '\nRGB Decimal factory'
-    parser.ResultFactory = DecimalFactory
-    parser.Components = 3
-    for cur in values:
-        print cur, '=>', parser.parse(cur)
-    
-    print '\nRGBA Float factory'
-    parser.ResultFactory = FloatFactory
-    parser.Components = 4
-    for cur in values:
-        print cur, '=>', parser.parse(cur)
